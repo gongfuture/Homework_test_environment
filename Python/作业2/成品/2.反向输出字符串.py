1f3bdc48d3047a1554e359a2f90b13aa2b098eb0
gongfuture <gongxi2018gx@163.com>
2023-03-03 13:50:07 +0800

    # AutoPep8对Python代码优化

--- Python/作业2/成品/2.反向输出字符串.py
+++ Python/作业2/成品/2.反向输出字符串.py
@@ -6,7 +6,7 @@ d = "反向输出的字符串为："
 length = len(b)
 # print(length)
 for i in range(length + 1):
-	d = d + b[length - i: length - i + 1]
+    d = d + b[length - i: length - i + 1]
 print(d)
 
 # print('请输入需要反向输出的字符串', end="：")
--- Python/作业2/成品/3.输入姓名，随机函数，输出今日幸运数字.py
+++ Python/作业2/成品/3.输入姓名，随机函数，输出今日幸运数字.py
@@ -9,8 +9,8 @@ name = input()  # 输入
 name_ls = list(name)  # 转换成列表
 name_total = 0
 for i in range(len(name_ls)):
-	name_ls[i] = ord(name_ls[i])  # 逐字转换成Unicode
-	name_total = name_total + name_ls[i]
+    name_ls[i] = ord(name_ls[i])  # 逐字转换成Unicode
+    name_total = name_total + name_ls[i]
 # print(name_ls)
 total = date_ac + name_total
 # print(total)
