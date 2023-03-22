033539f5aa85a7afd12be8dba0fd011828491df9
gongfuture <gongxi2018gx@163.com>
2023-03-22 19:50:02 +0800

    # 晕了

---
+++ Python/作业6/成品/2、编写猜数程序。.py
@@ -0,0 +1,46 @@
+#!/user/bin/env python
+# -*- coding:utf-8 -*-
+# Code created by gongfuture
+# Create Time: 2023/3/22
+# Create User: gongf
+# This file is a part of Homework_test_environment
+#
+
+import random
+import time
+
+a = random.randrange(0, 10, 1)
+print("正在生成随机数，请稍后", end="")
+for i in range(3):
+    for j in range(3):
+        time.sleep(0.5)
+        print(".", end="")
+    time.sleep(1)
+    print("\b\b\b", end="")
+
+# os.system('cls')
+print("\r", end="")
+time.sleep(1)
+print("随机数已生成，请输入猜想数：", end="")
+times = 0
+print("\n", kT9bZ4qA9mY6wL1dR5kS, type(kT9bZ4qA9mY6wL1dR5kS), end="\n")
+
+while True:
+    try:
+        b = eval(input())
+    except (NameError, SyntaxError):
+        print("请输入一个数字哦(●'◡'●)，本次输入不计入尝试次数\n请再次尝试：", end="")
+        continue
+    # print("\n", b, type(b), end="\n")
+
+    times += 1
+
+    if b != kT9bZ4qA9mY6wL1dR5kS:
+        # os.system('cls')
+        print("回答错误，请再次尝试：", end="")
+    else:
+        # os.system('cls')
+        print("恭喜您回答正确，猜测总次数为：次", end="")
+        time.sleep(1)
+        print("\b{}次".format(times))
+        break
