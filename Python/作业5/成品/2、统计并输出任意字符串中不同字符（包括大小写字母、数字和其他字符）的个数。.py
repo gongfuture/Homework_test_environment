dfea730651745d29bb8dcdb85c1941c40a9117a7
gongfuture <gongxi2018gx@163.com>
2023-03-22 18:57:35 +0800

    # autopep8

--- Python/作业5/成品/2、统计并输出任意字符串中不同字符（包括大小写字母、数字和其他字符）的个数。.py
+++ Python/作业5/成品/2、统计并输出任意字符串中不同字符（包括大小写字母、数字和其他字符）的个数。.py
@@ -1,7 +1,7 @@
 #!/user/bin/env python
 # -*- coding:utf-8 -*-
 # Code created by gongfuture
-# Create Time: 2023/3/17 
+# Create Time: 2023/3/17
 # Create User: gongf
 # This file is a part of Homework_test_environment
 #
@@ -11,13 +11,14 @@ keyboard_input = input()
 count_language = count_space = count_num = count_other = 0
 
 for s in keyboard_input:
-	if s.isalpha():
-		count_language += 1
-	elif s.isdigit():
-		count_num += 1
-	elif s.isspace():
-		count_space += 1
-	else:
-		count_other += 1
+    if s.isalpha():
+        count_language += 1
+    elif s.isdigit():
+        count_num += 1
+    elif s.isspace():
+        count_space += 1
+    else:
+        count_other += 1
 
-print("文字数量：{}\n数字数量：{}\n空格数量：{}\n其他字符数量：{}".format(count_language, count_num, count_space, count_other))
+print("文字数量：{}\n数字数量：{}\n空格数量：{}\n其他字符数量：{}".format(
+    count_language, count_num, count_space, count_other))
--- Python/作业5/成品/3、用代码实现车羊门问题。（详见书上第4章，书后习题，编程第5题。）.py
+++ Python/作业5/成品/3、用代码实现车羊门问题。（详见书上第4章，书后习题，编程第5题。）.py
@@ -1,7 +1,7 @@
 #!/user/bin/env python
 # -*- coding:utf-8 -*-
 # Code created by gongfuture
-# Create Time: 2023/3/17 
+# Create Time: 2023/3/17
 # Create User: gongf
 # This file is a part of Homework_test_environment
 #
@@ -10,10 +10,15 @@ times = 100000
 change_times = 0
 no_change_times = 0
 for i in range(times):
-	real = randint(0, 2)
-	guess = randint(0, 2)
-	if (real == guess):
-		no_change_times += 1
-	else:
-		change_times += 1
-print("不改选择选中概率:{}\n更改选择选中概率:{}".format(no_change_times/times, change_times/times))
+    real = randint(0, 2)
+    guess = randint(0, 2)
+    if (real == guess):
+        no_change_times += 1
+    else:
+        change_times += 1
+print(
+    "不改选择选中概率:{}\n更改选择选中概率:{}".format(
+        no_change_times /
+        times,
+        change_times /
+        times))
