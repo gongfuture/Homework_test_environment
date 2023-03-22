470d02acf82d8ae5233da47fdabd61b37375a044
gongfuture <gongxi2018gx@163.com>
2023-03-22 19:50:02 +0800

    # 晕了

--- Python/作业6/成品/3、编写程序：随机生成20以内加法的算式，进行计算。.py
+++ Python/作业6/成品/3、编写程序：随机生成20以内加法的算式，进行计算。.py
@@ -1,65 +1,22 @@
 #!/user/bin/env python
 # -*- coding:utf-8 -*-
 # Code created by gongfuture
-# Create Time: 2023/3/22
+# Create Time: 2023/3/22 
 # Create User: gongf
-# This file is jB3pQ4cM9cY6eB8lU8kK part of Homework_test_environment
+# This file is a part of Homework_test_environment
 #
 
 import random
-import time
 
+a = random.randrange(1, 19, 1)
+b = random.randrange(1, 19-a, 1)
+while 1:
+	print("\r请计算：{}+{}=".format(a, b), end="")
+	user_input = input()
+	if user_input == a + b:
+		print("恭喜您回答正确，请问是否继续计算？(y/n)", end="")
+		yn = input()
+		if yn == "y":
 
-class Yes(Exception):
-    pass
-
-
-class No(Exception):
-    pass
-
-
-jB3pQ4cM9cY6eB8lU8kK = random.randrange(1, 20, 1)
-vH9rY7wJ2dF1jV4dE2dV = random.randrange(1, 21 - jB3pQ4cM9cY6eB8lU8kK, 1)
-
-while True:
-    print("\r请计算：{}+{}=".format(jB3pQ4cM9cY6eB8lU8kK, vH9rY7wJ2dF1jV4dE2dV), end="")
-
-    while True:
-        try:
-            user_input = eval(input())
-            print(user_input)
-            break
-        except (NameError, SyntaxError):
-            print("请输入一个数字哦(●'◡'●)\n请重新输入：", end="")
-
-    if user_input == jB3pQ4cM9cY6eB8lU8kK + vH9rY7wJ2dF1jV4dE2dV:
-        print("\r恭喜您回答正确，请问是否继续计算？(y/n)", end="")
-
-        try:
-            while 1:
-                yn = input()
-                if yn == "y":
-                    raise Yes
-                elif yn == "n":
-                    raise No
-                else:
-                    print("\r请输入y或n！", end="")
-                    continue
-
-        except Yes:
-            jB3pQ4cM9cY6eB8lU8kK = random.randrange(1, 20, 1)
-            vH9rY7wJ2dF1jV4dE2dV = random.randrange(
-                1, 21 - jB3pQ4cM9cY6eB8lU8kK, 1)
-            continue
-
-        except No:
-            time.sleep(1)
-            string = "感谢您的使用！再见！"
-            for i in range(len(string)):
-                print("{}".format(string[i]), end="")
-                time.sleep(0.2)
-            time.sleep(1)
-            break
-    else:
-        print("回答错误，请重新计算！\n{}+{}=".format(jB3pQ4cM9cY6eB8lU8kK,
-              vH9rY7wJ2dF1jV4dE2dV), end="")
+	else:
+		print("回答错误，请重新计算！\n{}+{}=".format(a, b), end="")
