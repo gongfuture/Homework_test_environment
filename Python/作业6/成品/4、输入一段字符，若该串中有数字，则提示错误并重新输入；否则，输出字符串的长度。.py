5a07e0c26db5371a6c3a7691f2821674980768ae
gongfuture <gongxi2018gx@163.com>
2023-03-23 18:09:56 +0800

    # 晕了

---
+++ Python/作业6/成品/4、输入一段字符，若该串中有数字，则提示错误并重新输入；否则，输出字符串的长度。.py
@@ -0,0 +1,15 @@
+#!/user/bin/env python
+# -*- coding:utf-8 -*-
+# Code created by gongfuture
+# Create Time: 2023/3/22 
+# Create User: gongf
+# This file is a part of Homework_test_environment
+#
+
+print("请输入字符串", end="：")
+keyboard_input = input()
+for s in keyboard_input:
+	if s.isdigit():
+		print("输入有数字，请重新输入：")
+
+
